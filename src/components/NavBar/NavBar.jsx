fe61d88fb332a8af664dda25e201b1faaacc0fa8
maticamisay <maticamisay99@hotmail.com>
2021-08-24 16:39:39 -0300

    Desafio 3 terminado, navbar con categorias

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,32 @@
+import Navbar from 'react-bootstrap/Navbar'
+import Container from 'react-bootstrap/Container'
+import Nav from 'react-bootstrap/Nav'
+import NavDropdown from 'react-bootstrap/NavDropdown'
+
+function NavBar() {
+    return (
+        <Navbar bg="light" expand="lg">
+            <Container>
+                <Navbar.Brand href="#home">Zeus Ecommerce</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="me-auto">
+                    <NavDropdown title="Impresión" id="basic-nav-dropdown">
+                        <NavDropdown.Item href="#action/3.1">Cartuchos</NavDropdown.Item>
+                        <NavDropdown.Item href="#action/3.2">Tintas</NavDropdown.Item>
+                        <NavDropdown.Item href="#action/3.3">Papel fotográfico</NavDropdown.Item>
+                    </NavDropdown>
+                    <NavDropdown title="Computación" id="basic-nav-dropdown">
+                        <NavDropdown.Item href="#action/3.1">Almacenamiento</NavDropdown.Item>
+                        <NavDropdown.Item href="#action/3.2">Periféricos</NavDropdown.Item>
+                        <NavDropdown.Item href="#action/3.3">Componentes</NavDropdown.Item>
+                    </NavDropdown>
+
+                </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    )
+}
+
+export default NavBar

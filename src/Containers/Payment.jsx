40fc7f583a16e6f03f266d8a3da2d30b776258a9
maticamisay <maticamisay99@hotmail.com>
2021-10-08 12:17:00 -0300

    integracion de mercado pago v1.0

--- src/Containers/Payment.jsx
+++ src/Containers/Payment.jsx
@@ -54,6 +54,39 @@ function Payment() {
       history.push("/checkout/success");
     }
   };
+
+  // SDK de Mercado Pago
+const mercadopago = require ('mercadopago');
+// Agrega credenciales
+mercadopago.configure({
+  access_token: 'TEST-6671287059027708-100813-b6e84f26d5ce63bfc3e78216be991bc3-174131484'
+});
+// Crea un objeto de preferencia
+// let preference = {
+//   items: [
+//     {
+//       title: 'Mi producto',
+//       unit_price: 100,
+//       quantity: 1,
+//     }
+//   ]
+// };
+  let preference={items:carrito.map((product)=>(
+    {title: product.item.name,
+    unit_price:product.item.price,
+    quantity:product.quantity}
+  ))}
+;
+// let preference = {items:[productos]};
+console.log(preference);
+mercadopago.preferences.create(preference)
+.then(function(response){
+// Este valor reemplazará el string "<%= global.id %>" en tu HTML
+  global.id = response.body.id;
+}).catch(function(error){
+  console.log(error);
+});
+
   return (
     <div className="Payment">
       <div className="Paymen-content">
